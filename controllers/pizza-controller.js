6b2b6b52aee5865503b7b0f8c4d0a99bc04bfa32
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-02-23 22:09:11 -0700

    comments - api, routes, table etc

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,15 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      // .populate populate comment field to display user comment instead of comment id
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      // Select method above and below omits the __v with the leading minus sign
+      .select("-__v")
+      // Sort method set to descending - order newest pizza first
+      .sort({ _id: -1 })
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
@@ -11,18 +20,22 @@ const pizzaController = {
       });
   },
 
-  // get one pizza by id
-  getPizzaById({ params }, res) {
+// get one pizza by id
+getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
-      .then((dbPizzaData) => {
-        // If no pizza is found, send 404
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbPizzaData => {
         if (!dbPizzaData) {
-          res.status(404).json({ message: "No pizza found with this id!" });
+          res.status(404).json({ message: 'No pizza found with this id!' });
           return;
         }
         res.json(dbPizzaData);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
         res.status(400).json(err);
       });
